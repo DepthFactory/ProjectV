5155dce30756fc1a332b67d053198586e62c0625
Ewakhine <wenell@hotmail.fr>
2015-05-15 18:43:11 +0200

    Add ProtoStruct and ProtoDoor.

---
+++ Assets/Scripts/OpenBasicDoor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenBasicDoor : MonoBehaviour {
+
+	public GameObject _Player;
+	public GameObject _Door;
+
+	void OnTriggerStay(Collider player)
+	{
+		if (player == _Player.GetComponent<Collider> ())
+			if (Input.GetKey (KeyCode.E))
+				_Door.transform.position += 0.2f * Vector3.forward;
+	}
+
+}
